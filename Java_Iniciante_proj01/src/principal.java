c89bd8902d4679bdfddc38501ae6faba295065e0
MarcosLemos-dev <dasilvalemosm@gmail.com>
2022-05-07 07:43:33 -0300

    criando variaveis do tipo inteiro e double

---
+++ Java_Iniciante_proj01/src/principal.java
@@ -0,0 +1,13 @@
+
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a , b; // variaveis do tipo inteiro
+		double resultado; //variaveis do tipo double
+		
+		
+	}
+
+}
