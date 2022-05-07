53b5b952000c5d8b17cfb007b34dfc3faee44350
MarcosLemos-dev <dasilvalemosm@gmail.com>
2022-05-07 08:02:36 -0300

    criando o programa simples que soma dois numeros digitados pelo usuario

--- Java_Iniciante_proj01/src/principal.java
+++ Java_Iniciante_proj01/src/principal.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner; // importando a classe scanner
 
 public class principal {
 
@@ -7,7 +7,16 @@ public class principal {
 		int a , b; // variaveis do tipo inteiro
 		double resultado; //variaveis do tipo double
 		
+		Scanner ler = new Scanner(System.in); //instanciando e criando um objeto Scanner
 		
+		System.out.println("digite o numero a: ");
+		a = ler.nextInt();
+		System.out.println("digite o numero b: ");
+		b = ler.nextInt();
+
+		resultado  = a + b;
+		System.out.println("o resultado da soma foi: " + resultado);
+
 	}
 
 }
